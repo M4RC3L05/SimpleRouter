2b93dba86e2049f1c8d7f72a57056f13d443b264
M4RC3L0 <joaobat@live.com.pt>
2019-01-29 14:47:25 +0000

    make tests pass

--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -13,8 +13,7 @@ class ResponseTest extends TestCase
     public function test_it_should_create_a_response_object()
     {
         try {
-            $viewEgMock = $this->createMock(ViewEngine::class);
-            $response = new Response($viewEgMock);
+            $response = new Response();
             $this->assertEquals(1, 1);
         } catch (\Exception $e) {
             $this->assertEquals(1, 2);
@@ -23,8 +22,7 @@ class ResponseTest extends TestCase
 
     public function test_it_should_make_a_json_response()
     {
-        $viewEgMock = $this->createMock(ViewEngine::class);
-        $response = new Response($viewEgMock);
+        $response = new Response();
         ob_start();
         $response->json(["a" => 1]);
         $json = ob_get_clean();
@@ -33,8 +31,7 @@ class ResponseTest extends TestCase
 
     public function test_it_should_make_a_string_html_response()
     {
-        $viewEgMock = $this->createMock(ViewEngine::class);
-        $response = new Response($viewEgMock);
+        $response = new Response();
         ob_start();
         $response->sendString("ola");
         $string = ob_get_clean();
@@ -43,37 +40,5 @@ class ResponseTest extends TestCase
         $html = ob_get_clean();
         $this->assertEquals("ola", $string);
         $this->assertEquals("<h1>ola</h1>", $html);
-    }
-
-    public function test_it_should_render_a_view()
-    {
-
-        $response = new Response(new ViewEngine(__DIR__ . "/views"));
-        \ob_start();
-        $response->withViewData(["users" => ["João", "Ana"]])->view("home/index.twig");
-        $res = \ob_get_clean();
-        $this->assertEquals(\str_replace(["\n", "\r", " "], "", "
-        <!DOCTYPE html>
-        <html lang=\"en\">
-        <head>
-                        <meta charset=\"UTF-8\">
-                <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-                <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">
-                <title>Home</title>
-            
-            <style type=\"text/css\">
-                .important { color: #336699; }
-            </style>
-        </head>
-        <body>
-                <h1>Index</h1>
-            <p>Users</p>
-                    <ul>
-                            <li>João</li>
-                            <li>Ana</li>
-                        </ul>
-            
-            </body>
-        </html>"), \str_replace(["\n", " "], "", $res));
     }
 }
